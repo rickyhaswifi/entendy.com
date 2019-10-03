4febee98c5adf672a77393024207b22e67aeb22b
Ricardo Bautista <rickyhaswifi@gmail.com>
2019-10-03 00:23:30 -0600

    added sqs page

---
+++ squarespace-podcast/main.js
@@ -0,0 +1,31 @@
+function linkFunction() {
+    var x = document.getElementById("frm1");
+    var text = "";
+    var i
+    for (i = 0; i < x.length ;i++) {
+            text += x.elements[i].value + "?format=rss";
+    }
+    document.getElementById("linkPrint").innerHTML = text;
+   }
+
+   // Setup the variables
+var textarea = document.getElementById("linkPrint");
+var answer  = document.getElementById("copyAnswer");
+var copy    = document.getElementById("copyBlock");
+copy.addEventListener('click', function(e) {
+event.preventDefault();
+// Select some text (you could also create a range)
+textarea.select();
+
+// Use try & catch for unsupported browser
+try {
+
+		// The important part (copy selected text)
+		var ok = document.execCommand('copy');
+
+		if (ok) answer.innerHTML = 'Copied!';
+		else    answer.innerHTML = 'Unable to copy!';
+} catch (err) {
+		answer.innerHTML = 'Unsupported Browser!';
+}
+});
